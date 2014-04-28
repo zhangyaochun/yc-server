743ee5d6c3f584b4bd5e8362826a48be5ab2d160
zhangyaochun <zhangyaochun@wandoujia.com>
2014-04-28 17:28:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+//core
+var http = require('http');
+
+//third part
+var open = require('yc-open');
+var connect = require('connect');
+var injectReload = require('connect-livereload');
+
+function prepareOpts(opts) {
+    opts.port = opts.port || 8000;
+    //default false
+    opts.livereload = opts.livereload || false;
+
+    //check if use connect logger middleware
+    opts.debug = opts.debug || false;
+
+    //default open
+    opts.open = opts.open || true;
+}
+
+
+module.exports = function(opts) {
+
+    opts = prepareOpts(opts);
+
+    var middleware = opts.middleware ? opts.middleware.call(this, connect, opts) : [];
+
+    //debug
+    if (opts.debug === true) {
+        middleware.unshift(connect.logger('dev'));
+    }
+
+
+    //livereload
+    if (opts.livereload === true) {
+        middleware.unshift(injectReload({
+            port: 35729
+        }));
+    }
+
+    var app = connect.apply(null, middleware)
+
+    var server = http.createServer(app);
+
+    server.listen(opts.port).on('listening', function(){
+        var address = server.address();
+
+        //fix open type
+        if (opts.open === true) {
+            open({
+                url: 'http://' + address.address + ':' + address.port
+            });
+        } else if (typeof opts.open === 'string') {
+            open({
+                url: opts.open
+            });
+        }
+
+    }).on('error', function(err){
+        if (err.code === 'EADDRINUSE') {
+            console.error('Port ' + opts.port + ' is already in use.');
+        }
+    });
+
+};
